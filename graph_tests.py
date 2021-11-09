f54247fd99cdbfabf64e12862f59d8ad2d21935d
Hoa Tran <hoatran142@outlook.com>
2021-11-09 16:46:52 +0700

    How to draw a pie chart with matplotlib

--- graph_tests.py
+++ graph_tests.py
@@ -1,14 +1,20 @@
 import matplotlib.pyplot as plt
 
+option_votes = [63, 28, 8]
+option_names = [
+    "Flask",
+    "Django",
+    "It depends"
+]
 
-# figure = plt.figure()
-# ax1 = figure.add_subplot(1, 2, 1)
-# ax2 = figure.add_subplot(1, 2, 2)
+figure = plt.figure()
+axes = figure.add_subplot()
 
-figure, (ax1, ax2) = plt.subplots(1, 2)
-
-ax1.plot([1, 2, 3, 4], [3, 5, 9, 25])
-ax2.plot([1, 2, 3, 4], [5, 7, 11, 17])
-
+axes.pie(
+    option_votes,
+    labels=option_names,
+    explode=[0.1, 0, 0],
+    autopct="%1.1f%%"
+)
 
 plt.show()
