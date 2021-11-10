553a2fec98e2d9122e0c88c76c0b1b5b0b400ecd
Hoa Tran <hoatran142@outlook.com>
2021-11-10 15:48:30 +0700

    How to export an image with matplotlib

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@ poll_women = [poll[2] for poll in polls]
 
 poll_x_coordinates = range(len(polls))
 
-figure = plt.figure(figsize=(6, 6))
+figure = plt.figure(figsize=(6, 6), linewidth=5)
 figure.subplots_adjust(bottom=0.35)
 axes = figure.add_subplot()
 men_plot = axes.bar(
@@ -25,3 +25,4 @@ axes.legend((men_plot, women_plot), ("Men", "Women"))
 plt.xticks(poll_x_coordinates, poll_titles, rotation=30, ha="right")
 
 plt.show()
+# plt.savefig("graph.png", bbox_inches="tight", edgecolor="#5c44fd")
